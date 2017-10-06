6fa506d4100728a9d3ebb2cd64503f490ba4dcf5
Ivan Rodriguez <jivanrod@gmail.com>
2017-10-06 16:55:28 +0200

    fix funciones.php

--- funciones.php
+++ funciones.php
@@ -1,28 +1,61 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        function checkEro($word) {
-            $pos = strpos($word, 'ero');
-            return ($pos && ($pos === strlen($word) - 3));
-        }
+<?php
+if (empty($_POST)) {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <link rel="stylesheet" href="stylesheet.css">
+            <title>Form Entrada Datos</title>
+        </head>
+        <body>
+            <h1> Formulario de Registro de Cliente </h1>
+            <div class="capaform">
 
-        $inputWord = "panadero";
+                <form class="form-font" name="Formregistro" 
+                      action="funciones.php" method="POST">
+                    <div class="form-section">
+                        <label for="input"> Nombre: </Label> 
+                        <input id="nombre" type="text" name="input" size="30" /> 
+                    </div>
+                    <input class="submit" type="submit" 
+                           value="Enviar" name="botonenvio" /> 
+                </form>
+            </div>
+        </body>
+    </html>
+    <?php
+} else {
 
-        if (checkEro($inputWord)) {
-            echo "<h1>La palabra $inputWord contiene 'ero' al final</h1>";
-        } else {
-            echo "<h1>La palabra $inputWord no contiene 'ero' al final</h1>";
-        }
-        ?>
-    </body>
-</html>
+    function checkEnding($word, $ending) {
+        $pos = strpos($word, $ending);
+        return (($pos !== false) && ($pos === strlen($word) - strlen($ending)));
+    }
+
+    $input = filter_input(INPUT_POST, 'input', FILTER_SANITIZE_STRING);
+    $words = str_word_count($input, 1);
+    
+    define('ENDING', "ero");
+    ?>
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset = "UTF-8">
+            <title>Info de palabra</title>
+        </head>
+        <body>
+            <?php
+            foreach ($words as $word) {
+                if (checkEnding($word, ENDING)) {
+                    echo "<h1>La palabra $word contiene " . ENDING . " al final</h1>";
+                } else {
+                    echo "<h1>La palabra $word no contiene " . ENDING . " al final</h1>";
+                }
+            }
+            ?>
+        </body>
+    </html>
+    <?php
+}
+
