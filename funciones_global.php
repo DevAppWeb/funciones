e9a6416932120f051d28038ca7bf25c3606f5c8b
Iván <jivanrod@gmail.com>
2021-10-20 15:10:54 +0200

    change code

--- funciones_global.php
+++ funciones_global.php
@@ -1,70 +1,57 @@
 <?php
-if (empty($_POST)) {
-    ?>
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link rel="stylesheet" href="stylesheet.css">
-            <title>Form Entrada Datos</title>
-        </head>
-        <body>
-            <h1>Entrada Datos</h1>
-            <div class="capaform">
-                <form class="form-font" name="Formregistro" 
-                      action="funciones_global.php" method="POST">
-                    <div class="form-section">
-                        <label for="input"> Cadena: </Label> 
-                        <input id="nombre" type="text" name="input" size="30" /> 
-                    </div>
-                    <input class="submit" type="submit" 
-                           value="Enviar" name="botonenvio" /> 
-                </form>
-            </div>
-        </body>
-    </html>
-    <?php
-} else {
+define('ENDING', "ero");
 
-    function checkAciertos($word, $str) {
-        static $aciertos = 0;
-        global $gameOver;
-        if ($aciertos >= 0) {
-            if ($aciertos < $gameOver) {
-                if (strpos($word, $str) !== false) {
-                    $aciertos++;
-                }
-            } else {
-                $aciertos = -1;
-            }
-        }
-        return ($aciertos);
+$gameOver = 5;
+
+function checkEnding($word, $ending) {
+    $pos = strpos($word, $ending);
+    return (($pos !== false) && ($pos === strlen($word) - strlen($ending)));
+}
+
+function countMatch($word, $ending) {
+    static $matches = 0;
+    if (checkEnding($word, $ending)) {
+        $matches++;
     }
+    return ($matches);
+}
 
-    define('STRING', "ero");
-    $gameOver = 2;
+function isGameOver ($matches) {
+    global $gameOver;
+    return ($matches >= $gameOver);
+}
+
+if (!empty($_POST)) {
     $input = filter_input(INPUT_POST, 'input', FILTER_SANITIZE_STRING);
     $words = str_word_count($input, 1);
-    ?>
-
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset = "UTF-8">
-            <title>Info de palabra</title>
-        </head>
-        <body>
-            <?php
-            foreach ($words as $word) {
-                $aciertos = checkAciertos($word, STRING);
-                if ($aciertos >= 0) {
-                    echo "<h1>Con la palabra $word se acumulan " . $aciertos . " aciertos </h1>";
-                } else {
-                    echo "<h1>Ya se han acumulado $gameOver aciertos</h1>";
-                }
-            }
-            ?>
-        </body>
-    </html>
-    <?php
+    foreach ($words as $word) {
+        $matches = countMatch($word, ENDING);
+    }
 }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="stylesheet.css">
+        <title>Form Entrada Datos</title>
+    </head>
+    <body>
+        <h1>Entrada Datos</h1>
+        <div class="capaform">
+            <form class="form-font" name="Formregistro" 
+                  action="funciones_global.php" method="POST">
+                <div class="form-section">
+                    <label for="input"> Cadena: </label> 
+                    <input id="nombre" type="text" name="input" size="30" /> 
+                </div>
+                <input class="submit" type="submit" 
+                       value="Enviar" name="botonenvio" /> 
+            </form>
+        </div>
+        <?php if (isset ($matches)): ?>
+            <h1><?= (!isGameOver($matches)) ? "Found " . $matches . " matches " : "Already got " . $gameOver . " matches " ?></h1>
+        <?php endif ?>
+
+    </body>
+</html>
--- funciones_ref.php
+++ funciones_ref.php
@@ -1,60 +1,43 @@
-<?php
-if (empty($_POST)) {
-    ?>
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link rel="stylesheet" href="stylesheet.css">
-            <title>Form Entrada Datos</title>
-        </head>
-        <body>
-            <h1>Entrada Datos</h1>
-            <div class="capaform">
-                <form class="form-font" name="Formregistro" 
-                      action="funciones_ref.php" method="POST">
-                    <div class="form-section">
-                        <label for="input"> Cadena: </Label> 
-                        <input id="nombre" type="text" name="input" size="30" /> 
-                    </div>
-                    <input class="submit" type="submit" 
-                           value="Enviar" name="botonenvio" /> 
-                </form>
-            </div>
-        </body>
-    </html>
-    <?php
-} else {
+<?php define('ENDING', "ero");
 
-    function checkStr($word, $str, &$pos) {
+function findEnding($word, $str, &$pos) {
         $pos = strpos($word, $str);
         return ($pos !== false);
     }
     
-    define('STRING', "ero");
 
+if (!empty($_POST)) {
     $input = filter_input(INPUT_POST, 'input', FILTER_SANITIZE_STRING);
     $words = str_word_count($input, 1);
-    
-    ?>
-
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset = "UTF-8">
-            <title>Info de palabra</title>
-        </head>
-        <body>
-            <?php
-            foreach ($words as $word) {
-                if (checkStr($word, STRING, $pos)) {
-                    echo "<h1>La palabra $word contiene " . STRING . " en la posición $pos</h1>";
-                } else {
-                    echo "<h1>La palabra $word no contiene " . STRING . " al final</h1>";
-                }
-            }
-            ?>
-        </body>
-    </html>
-    <?php
 }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="stylesheet.css">
+        <title>Form Entrada Datos</title>
+    </head>
+    <body>
+        <h1>Entrada Datos</h1>
+        <div class="capaform">
+            <form class="form-font" name="Formregistro" 
+                  action="funciones_ref.php" method="POST">
+                <div class="form-section">
+                    <label for="input"> Cadena: </label> 
+                    <input id="nombre" type="text" name="input" size="30" /> 
+                </div>
+                <input class="submit" type="submit" 
+                       value="Enviar" name="botonenvio" /> 
+            </form>
+        </div>
+        <?php if (isset($words)): ?>
+            <div>
+                <?php foreach ($words as $word): ?>
+                    <h1><?= (findEnding($word, ENDING, $pos)) ? "La palabra \"$word\" contiene " . ENDING . " en la posición " . $pos+1 : "La palabra $word no contiene " . ENDING . " al final" ?></h1>
+                <?php endforeach ?>
+            </div>
+        <?php endif ?>
+    </body>
+</html>
+
